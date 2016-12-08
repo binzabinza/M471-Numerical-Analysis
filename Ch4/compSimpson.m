9efba50d30a2e904402696b672a53a223e80d7b3
John Francis Binzer <jfbinzer@silo.soic.indiana.edu>
2016-12-08 04:56:21 -0500

    Composite Simpson

---
+++ Ch4/compSimpson.m
@@ -0,0 +1,20 @@
+%John Binzer
+%Composite Simpson's Rule
+
+function compSimpson(a, b, n, f)
+    %endpoints a,b; even positive integer m; function f
+    h = (b-a)/n;
+    XI0 = f(a) + f(b);  
+    XI1 = 0;            %Summation of f(x(2i-1))
+    XI2 = 0;            %Summation of f(x(2i))
+    
+    for i=1:n
+        X = a+i*h;
+        if (mod(i, 2)==0)
+            XI2 = XI2 + f(X);
+        else
+            XI1 = XI1 + f(X);
+        end
+    end
+    
+    XI = h*(XI0+2*XI2+4*XI1)/3
